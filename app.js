2381cf0521abe3fbf8717ab5e927ce7ace2d8c38
Esteban Almiron <estebanalmiron1@gmail.com>
2023-07-25 20:05:30 -0300

    Cambie app.listen en app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('El servidor inicio correctamente'));
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
